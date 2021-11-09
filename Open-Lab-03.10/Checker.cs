47bb296c5dd9c44633abcd9628d57d5430a00e8a
z20ist59 <msmidovic@gmail.com>
2021-11-09 09:49:33 +0100

    rovnake pismeno

--- Open-Lab-03.10/Checker.cs
+++ Open-Lab-03.10/Checker.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Open_Lab_03._10
 {
@@ -6,7 +7,10 @@ namespace Open_Lab_03._10
     {
         public int GetNumberOfCharsInString(char letter, string str)
         {
-            throw new NotImplementedException();
+            char ch = letter;
+            int freq = str.Count(f => (f == ch));
+            return freq;
+
         }
     }
 }
